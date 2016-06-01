adf6639f82994c237e6b044ee0ef9ddad1da754f
MtGam <theresm@gmail.com>
2016-06-01 15:11:02 -0400

    banana reminder on list

--- exercise6.rb
+++ exercise6.rb
@@ -4,4 +4,10 @@ grocery_list.each do |grocery|
   puts "* #{grocery}"
 end
 
-puts "There are #{grocery_list.length} total items on my list."
+puts "There are #{grocery_list.length} items on my list."
+
+if grocery_list == "bananas"
+  puts "You don't need to pick up bananas today."
+else
+  puts "You need to pick up bananas."
+end
