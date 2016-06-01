5ef4bcc6f81f983745a916a915fcc047e3f321bf
MtGam <theresm@gmail.com>
2016-06-01 16:30:36 -0400

    list sorted alphabetically

--- exercise6.rb
+++ exercise6.rb
@@ -1,15 +1,24 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 grocery_list << "rice"
-grocery_list.each do |grocery|
-  puts "* #{grocery}"
+def display_list(list)
+  list.each do |grocery|
+    puts "* #{grocery}"
+  end
 end
+display_list(grocery_list)
+
+
 
 puts "There are #{grocery_list.length} items on my list."
 
 if grocery_list == "bananas"
-  puts "You don't need to pick up bananas today."
-else
   puts "You need to pick up bananas."
+else
+  puts "You don't need to pick up bananas today."
 end
 
 puts "The second item on the list is #{grocery_list[1]}."
+
+display_list(grocery_list.sort)
+
+grocery_list.delete_at(3)
